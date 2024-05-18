c4e4616fa26234f9aaf0d076dbe3dbcc5effcb11
slpuppy <puppizin@gmail.com>
2024-05-18 12:00:27 -0300

    added loader indicator to image extnsion

--- SwiftNews/Extensions/UIImageView + Extension.swift
+++ SwiftNews/Extensions/UIImageView + Extension.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 extension UIImageView {
-    func downloaded(from url: URL) {
+    func downloaded(from url: URL, placeholder: UIImage? = nil) {
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.center = CGPoint(x: self.bounds.midX, y: self.bounds.midY)
+        activityIndicator.hidesWhenStopped = true
+        DispatchQueue.main.async {
+            self.image = placeholder
+            self.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+        }
+        
         URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                activityIndicator.stopAnimating()
+                activityIndicator.removeFromSuperview()
+            }
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
                 let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
                 let data = data, error == nil,
                 let image = UIImage(data: data)
-                else { return }
+            else { return }
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
             }
